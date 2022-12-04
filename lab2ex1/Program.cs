0ca978f9ded4dea92cb708f57ddccf3a38bab71e
Poptamas Vlad <poptamasvlad@gmail.com>
2022-12-04 14:52:49 +0200

    Add project files.

---
+++ lab2ex1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lab2ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*: Scrieti un program care va afisa suma cifrelor unui numar n, n fiind
+            citit de la tastatura.*/
+
+            int n = int.Parse(Console.ReadLine());
+            int suma = 0;
+            while(n != 0)
+            {
+                suma = suma + n % 10;
+                n = n / 10;
+            }
+            Console.WriteLine(suma);
+
+        }
+    }
+}
